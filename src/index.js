5219456d6c5a7e315f8aa637fbe730cba481a6d5
Dani <meharidaniel1111@gmail.com>
2020-04-21 13:19:28 +0200

    my blog sampl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Blog(props) {
+    const sidebar = (
+    <ul>
+        {props.posts.map((post)=> <li key= { post.id }> { post.title} </li>)}
+    </ul>
+    )
+    
+    const content = (props.posts.map((post)=>
+         <div key= {post.id}>
+            <h2> {post.title} </h2>
+            <p> { post.content } </p>
+        </div>
+        )
+        
+    )
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    )
+}
 
+const posts = [
+    {id: 1, title: "react", content: "React is the fronend library"},
+    {id: 2, title: "Angular", content: "React is the fronend library"}
+];
+
+ReactDOM.render(<Blog posts= {posts} />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
